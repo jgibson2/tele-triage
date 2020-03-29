dcd493420b623a011a2b9aa2dc8e3077342fc421
Kevin Li <kevinli@kevins-mbp.attlocal.net>
2020-03-28 21:30:48 -0500

    matches with weights kl

--- matching/match_users.py
+++ matching/match_users.py
@@ -1,34 +1,52 @@
 import requests
-
 import pandas as pd
 import numpy as np
 import re
 import pandas as pd
 import json
 import pprint
+import sys
+from sklearn import preprocessing
+from matching.util_functions.round_robin import round_robin
+from matching.util_functions.extra_functions import convert_json_to_df
 
-from matching.round_robin import round_robin
-from matching.extra_functions import convert_json_to_df
 
 
 
-
-def get_match_weights(user_zipcode, assignment, selected_hospitals):
+def get_matches(user_zipcode, assignment, selected_hospitals, weights={'beds':0.2, 'distance':0.8,'level': 1}):
     df = convert_json_to_df(selected_hospitals)
-    round_robin_results = round_robin(user_zipcode, assignment, df)
+    round_robin_results = round_robin(user_zipcode, assignment, df, weights)
 
     res = list(round_robin_results['WEIGHTED_MATCH'])
-
+    print(res)
     return res
 
 
+## Testing
+def test_match_users():
+    with open('original_data/Hospitals.geojson') as fp:
+        selected_hospitals = json.load(fp)
 
+    ## Test 1
+    user_zipcode = 63112 # St.Louis
+    assignment = ['Level 1','Pediatric','Children']
+    weights = {
+        'beds': 0.5,
+        'distance': 0.5,
+    }
+    get_matches(user_zipcode, assignment, selected_hospitals, weights)
 
+    ## Test 2
+    user_zipcode = 97035 # Portland, OR
+    assignment = ['Level 2','Pediatric','Trauma']
+    weights = {
+        'beds': 0.3,
+        'distance': 0.7,
+    }
 
+    get_matches(user_zipcode, assignment, selected_hospitals, weights)
 
-## TEST CASES
-# with open('Hospitals.geojson') as fp:
-#     selected_hospitals = json.load(fp)
-# user_zipcode = 63112 # St.Louis
-# assignment = ['Level 1','Pediatric','Children']
-# get_matches(user_zipcode, assignment, selected_hospitals)
+    ## Test 3
+    user_zipcode = 33125 # Miami, FL
+    assignment = ['Level 1','Military']
+    get_matches(user_zipcode, assignment, selected_hospitals)
---
+++ matching/util_functions/extra_functions.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np  
+import json
+import re
+import requests
+from pandas.io.json import json_normalize
+
+
+
+
+
+def transform_trauma(trauma):
+    t = re.sub(r'[^A-Za-z0-9 ]+', '', trauma)
+    res = re.sub(r'\b' + 'I' + r'\b', '1', t)
+    res = re.sub(r'\b' + 'II' + r'\b', '2', res)
+    res = re.sub(r'\b' + 'III' + r'\b', '3', res)
+    res = re.sub(r'\b' + 'IV' + r'\b', '4', res)
+    res = re.sub(r'\b' + 'V' + r'\b', '5', res)
+    return res
+
+
+
+def get_distance(curr_location, hospital_location):
+    ## Haversine formula
+    long1 = curr_location[0] * np.pi /180
+    long2 = hospital_location[0] * np.pi /180
+
+    lat1 = curr_location[1] * np.pi /180
+    lat2 = hospital_location[1] * np.pi /180
+
+    r = 6356.752
+
+    inside_term = np.power(np.sin((lat2 - lat1)/2),2) + np.cos(lat1)*np.cos(lat2)*np.power(np.sin((long2-long1)/2),2)
+
+    dist = 2 * r * np.arcsin((np.power(inside_term,0.5))) / 1.609 # in miles
+
+    return dist
+
+def get_match_trauma(assignment, trauma):
+    if (trauma == 'NOT AVAILABLE'): return 1
+
+    for criteria in assignment:
+        if criteria.lower() in trauma.lower():
+            return 1
+
+    return 0
+
+def get_match_type(assignment, visit_type):
+
+    for criteria in assignment:
+        if criteria.lower() in visit_type.lower():
+            return 1
+    return 0
+
+def get_user_lat_long(user_zipcode):
+    url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q={user_zipcode}&facet=state&facet=timezone&facet=dst'.format(user_zipcode=user_zipcode)
+    response = requests.get(url)
+    
+    res = response.json()
+    latitude = res['records'][0]['fields']['latitude']
+    longitude = res['records'][0]['fields']['longitude']
+    return (longitude,latitude)
+
+
+def convert_json_to_df(data):
+    data = data['features']
+    data = json_normalize(data)[0:10]
+
+    df = pd.DataFrame(data)
+    df.drop(['type'], axis=1, inplace=True)
+
+    cols = list(df.columns)
+    cols = [x.split('.')[1] for x in cols]
+    df.columns = cols
+
+    df['X'] = df['coordinates'].apply(lambda row: row[0])
+    df['Y'] = df['coordinates'].apply(lambda row: row[1])
+    df.drop(['type', 'coordinates','OBJECTID'], axis=1, inplace=True)
+
+
+    return df
+
